f163aff8ab14227c701e0c8097db5ffd2d823a9e
shafiqalshaar <shafiqalshaar@gmail.com>
2025-06-17 15:13:43 +0300

    Add logo

--- resources/js/components/AppLogo.vue
+++ resources/js/components/AppLogo.vue
@@ -9,7 +9,7 @@ const teamName = computed(() => page.props.auth.team?.name || 'Company Name');
 
 <template>
     <div class="flex aspect-square size-8 items-center justify-center rounded-md bg-sidebar-primary text-sidebar-primary-foreground">
-        <AppLogoIcon class="size-5 fill-current text-white dark:text-black" />
+        <AppLogoIcon class="size-5 text-white dark:text-black" />
     </div>
     <div class="ml-1 grid flex-1 text-left text-sm">
         <span class="mb-0.5 truncate leading-tight font-semibold">{{ teamName }}</span>
