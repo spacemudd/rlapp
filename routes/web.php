b099e3a518ca9aa0a11c3d82de0b60e24a93e81d
shafiqalshaar <shafiqalshaar@gmail.com>
2025-06-18 14:57:30 +0300

    URL fix

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,7 @@ Route::get('/', function () {
 })->name('home');
 
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/', function () {
+    Route::get('/dashboard', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
 
