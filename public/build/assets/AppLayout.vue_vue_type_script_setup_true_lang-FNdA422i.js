9585408e51914c0eddc5608ba69dbd6cf80b451a
shafiqalshaar <shafiqalshaar@gmail.com>
2025-06-28 23:43:41 +0300

    auto login for local dev

---
+++ public/build/assets/AppLayout.vue_vue_type_script_setup_true_lang-FNdA422i.js
@@ -0,0 +1,1 @@
+import{e as r}from"./AppSidebarLayout.vue_vue_type_script_setup_true_lang-BXa7DggV.js";import{d as a,m as s,o,e as t,D as m}from"./app-dzhYHfQ6.js";const d=a({__name:"AppLayout",props:{breadcrumbs:{default:()=>[]}},setup(p){return(e,c)=>(o(),s(r,{breadcrumbs:e.breadcrumbs},{default:t(()=>[m(e.$slots,"default")]),_:3},8,["breadcrumbs"]))}});export{d as _};
