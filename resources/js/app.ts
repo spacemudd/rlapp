8d78a168647e1556720aebebb5da2686d1f67dc4
shafiqalshaar <shafiqalshaar@gmail.com>
2025-07-19 00:45:36 +0300

    Add some css

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,4 +1,5 @@
 import '../css/app.css';
+import 'vue3-toastify/dist/index.css';
 
 import { createInertiaApp } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
