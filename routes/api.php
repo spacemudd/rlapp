6643f68d8c84ddfd2742b56a713dcd5060dc2dee
Abdelrahman <abdelrahman.yousef@hadaf-hq.com>
2025-07-22 16:23:23 +0300

    test api reservation

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\Api\VehicleApiController;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ReservationApiController;
+use App\Http\Controllers\Api\TestReservationApiController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,10 +16,33 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware(['api.key'])->group(function () {
-    // Vehicle API endpoints
-    Route::prefix('vehicles')->group(function () {
-        Route::get('/', [VehicleApiController::class, 'index'])->name('api.vehicles.index');
-        Route::get('/{id}', [VehicleApiController::class, 'show'])->name('api.vehicles.show');
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+// Public API Routes (بدون CSRF protection)
+Route::prefix('v1')->group(function () {
+    // Reservations API
+    Route::prefix('reservations')->group(function () {
+        Route::get('/', [ReservationApiController::class, 'index']);
+        Route::post('/', [ReservationApiController::class, 'store']);
+        Route::get('/statistics', [ReservationApiController::class, 'statistics']);
+        Route::get('/today', [ReservationApiController::class, 'today']);
+        Route::get('/tomorrow', [ReservationApiController::class, 'tomorrow']);
+        Route::get('/available-vehicles', [ReservationApiController::class, 'availableVehicles']);
+        Route::get('/search', [ReservationApiController::class, 'search']);
+        Route::get('/status/{status}', [ReservationApiController::class, 'byStatus']);
+        Route::get('/{id}', [ReservationApiController::class, 'show']);
+        Route::put('/{id}', [ReservationApiController::class, 'update']);
+        Route::delete('/{id}', [ReservationApiController::class, 'destroy']);
+        Route::patch('/{id}/status', [ReservationApiController::class, 'updateStatus']);
     });
-}); 
+
+    // Test API endpoints (بدون authentication للاختبار)
+    Route::prefix('test')->group(function () {
+        Route::get('/data', [TestReservationApiController::class, 'testData']);
+        Route::get('/reservations', [TestReservationApiController::class, 'index']);
+        Route::post('/reservations', [TestReservationApiController::class, 'store']);
+        Route::post('/custom-reservation', [TestReservationApiController::class, 'createCustom']);
+    });
+});
